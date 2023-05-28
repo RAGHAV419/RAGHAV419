865441a233194736c61d9c1d1c9aae3d7f667b04
RAGHAV BHARDWAJ <127624992+RAGHAV419@users.noreply.github.com>
2023-05-28 22:45:38 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,13 @@
+package OOPS_Object_Oriented_Programming;
+
+public class student {
+
+	String name;
+	int age;
+	
+	public void intro() {
+		String name="duplicate";
+		System.out.println(this.name+" and "+this.age);
+	}
+
+}
