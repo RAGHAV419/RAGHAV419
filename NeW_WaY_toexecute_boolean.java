a0c88811e207f8329cad8b8a6494a83764d7f029
RAGHAV BHARDWAJ <127624992+RAGHAV419@users.noreply.github.com>
2023-07-13 14:36:47 +0530

    Add files via upload

---
+++ NeW_WaY_toexecute_boolean.java
@@ -0,0 +1,31 @@
+package SyLlAbUs;
+
+public class NeW_WaY_toexecute_boolean {
+
+	// THIS IS ALSO A WAY TO EXECUTE BOOELAN FUBNCTIONS IN THIS METHOD WE SIMPLY USE DOUBLE EQUALS TO AND IF THE VALUE COMES OUT TYO BE SAME AS IT IS 
+	// IN  THE MAIN FUNCTION THEN IT GIVES TRUE OTHERWISE FALSE
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		
+		System.out.println(tryy(i));
+		
+		System.out.println(trye(i));
+		
+	}
+	
+	public static boolean tryy(int i) {
+	
+		return i==0;
+		
+	}
+	
+	public static boolean trye(int i) {
+	
+		return i==5;
+		
+	}
+
+	
+
+}
